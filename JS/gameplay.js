1f4e91be9365f22cf90a4f6e591ac44874925d28
Erin V <erinkvoelker@gmail.com>
2022-06-15 20:50:09 -0400

    created main JS file for quiz elements & started constructing how quiz will run

---
+++ JS/gameplay.js
@@ -0,0 +1,14 @@
+var landingPage = document.querySelector(".landing-page");
+var quizIntro = document.querySelector("#quizIntro");
+var timer = document.querySelector(".timer");
+var playButton = document.querySelector("#play-btn");
+var quizElement = document.querySelector(".quiz");
+var quizQuestion = document.querySelector("#quiz-question");
+var multipleChoices = document.querySelector(".multiple-choices");
+var rightOrWrong = document.querySelector("#right-or-wrong");
+var quizFinished = document.querySelector("#quiz-finished");
+var finalScore = document.querySelector("#final-score");
+var playerInitials = document.querySelector("#player-initials");
+var submitScore = document.querySelector("#submit");
+
+console.log(x, y);

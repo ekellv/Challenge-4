73f77803a77a56e1a1d7faaf7b74523862a28330
Erin V <erinkvoelker@gmail.com>
2022-06-19 14:16:32 -0400

    fixed issue with quiz incorrectly running, deleted test html pages, deleted original JS file with confusing function names

--- JS/questions.js
+++ JS/questions.js
@@ -1,142 +1,85 @@
-    // var questions = [
-    //     {
-    //         title: "Inside which HTML element do we put the Javascript?",
-    //         choices:["<script>", 
-    //                 "<scripting>", 
-    //                 "<javascript>", 
-    //                 "<js>"]
-    //         answer: "<script>"
-    //     },
-    //     {
-    //         title: "What's the correct syntax to change the content of this piece of HTML: <p id=\"demo\">This is a demonstration></p>",
-    //         choices:["document.getElementById("demo").innerHTML = "Hello World!";", 
-    //                 "#demo.innerHTML = "Hello World!";", 
-    //                 "document.getElement("p").innerHTML = "Hello World!";", 
-    //                 "document.getElementByName("p").innerHTML = "Hello World!";"]
-    //         answer: "document.getElementById("demo").innerHTML = "Hello World!";"
-    //     },
-    //     {
-    //         title: "How do you write "Hello World" in an alert box?",
-    //         choices:["alertBox("Hello World");", 
-    //                 "alert("Hello World");", 
-    //                 "msgBox("Hello World");", 
-    //                 "msg("Hello World");"]
-    //         answer: "alert("Hello World");"
-    //     },
-    //     {
-    //         title: "How would you call a function named "myFunction"?",
-    //         choices:["call function myFunction()", 
-    //                 "call myFunction()", 
-    //                 "myFunction()", 
-    //                 "function myVar()"]
-    //         answer: "myFunction()"
-    //     },
-    //     {
-    //         title: "How do you write an IF statement in JavaScript?",
-    //         choices:["if i == 5 then", 
-    //                 "if i = 5 then", 
-    //                 "if i = 5", 
-    //                 "if (i == 5)"]
-    //         answer: "if (i == 5)"
-    //     },
-    //     {
-    //         title: "How would you write an IF statement for executing some code if "i" is NOT equal to 5?",
-    //         choices:["if i =! 5 then", 
-    //                 "if (i <> 5)", 
-    //                 "if i <> 5", 
-    //                 "if (i != 5)"]
-    //         answer: "if (i != 5)"
-    //     },
-    //     {
-    //         title: "How does a FOR loop start?",
-    //         choices:["for (i <= 5; i++)", 
-    //                 "for (i = 0; i <= 5; i++)", 
-    //                 "for (i = 0; i <= 5)", 
-    //                 "for i = 1 to 5"]
-    //         answer: "for (i = 0; i <= 5; i++)"
-    //     },
-    //     {
-    //         title: "How can you add a comment in a JavaScript file?",
-    //         choices:["//This is a comment", 
-    //                 "<!--This is a comment-->", 
-    //                 "'This is a comment", 
-    //                 "You are unable to add comments in JavaScript files, they must all be in a README.md."]
-    //         answer: "//This is a comment"
-    //     },
-    //     {
-    //         title: "What is the correct way to write a JavaScript array?",
-    //         choices:["var colors = ["red", "green", "blue"]", 
-    //                 "var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")", 
-    //                 "var colors = "red", "green", "blue"", 
-    //                 "var colors = (1:"red", 2:"green", 3:"blue")"]
-    //         answer: "var colors = ["red", "green", "blue"]"
-    //     },
-    //     {
-    //         title: "How do you round the number 7.25, to the nearest integer?",
-    //         choices:["round(7.25)", 
-    //                 "rnd(7.25)", 
-    //                 "\Math.round(7.25)", 
-    //                 "Math.rnd(7.25)"]
-    //         answer: "\Math.round(7.25)"
-    //     },
-    //     {
-    //         title: "Which event occurs when the user clicks on an HTML element?",
-    //         choices:["\onclick", 
-    //                 "onmouseclick", 
-    //                 "onchange", 
-    //                 "Onmouseover"]
-    //         answer: "\onclick"
-    //     },
-    //     {
-    //         title: "How do you declare a JavaScript variable?",
-    //         choices:["variable carName;", 
-    //                 "\var carName;", 
-    //                 "v carName;", 
-    //                 "carName()"]
-    //         answer: "\var carName;"
-    //     },
-    //     {
-    //         title: "Which operator is used to assign a value to a variable?",
-    //         choices:["x", 
-    //                 "*", 
-    //                 "-", 
-    //                 "="]
-    //         answer: "="
-    //     },
-    // ]
-    // //    {
-    // //        title: "";
-    // //        choices:["", 
-    // //                "", 
-    // //               "", 
-    // //                ""];
-    // //        answer: ""
-    // //    },
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var questions = [
+    {
+      title: "Which one is a looping structure in JavaScript?",
+      choices: ["All the below", "For", "While", "do-while loops"],
+      answer: "All the below"
+    },
+    {
+      title: "What are the two basic groups of data types in JavaScript?",
+      choices: [
+        "Primitive and attribute",
+        "Primitive and reference types",
+        "Reference types and attribute",
+        "None of the above"
+      ],
+      answer: "Primitive and reference types"
+    },
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "Boolean operators that can be used in JavaScript include:",
+      choices: [
+        "'And' Operator &&",
+        "'Or' Operator ||",
+        "'Not' Operator !",
+        "All the above"
+      ],
+      answer: "All the above"
+    },
+    {
+      title:
+        "Which one of these is not among the three different types of errors in JavaScript?",
+      choices: [
+        "Animation time errors",
+        "Load time errors",
+        "Run time errors",
+        "Logical Errors"
+      ],
+      answer: "Animation time errors"
+    },
+    {
+      title: "What is the data type of variables in JavaScript?",
+      choices: [
+        "Object data types",
+        "Function data type",
+        "None of the above",
+        "All of the above"
+      ],
+      answer: "Object data types"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store ____.",
+      choices: [
+        "numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"
+      ],
+      answer: "all of the above"
+    },
+    {
+      title:
+        "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      answer: "quotes"
+    },
+    {
+      title:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+    },
+    {
+      title: "What is the type of Pop up boxes available in JavaScript?:",
+      choices: ["Alert", "Confirm", "Prompt", "All the above"],
+      answer: "All the above"
+    }
+  ];

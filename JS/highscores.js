566ba1f7d40a103274687c4562ed5818b6ae91fb
Erin V <erinkvoelker@gmail.com>
2022-06-19 21:46:51 -0400

    cleared up confusing function name, still attempting to give gold to first user on high scores

--- JS/highscores.js
+++ JS/highscores.js
@@ -1,29 +1,33 @@
 function printHighscores() {
-  // either get scores from localstorage or set to empty array
+  // grab the high scores from what was stored in local storage post-quiz, or just create an empty array
   var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
 
-  // sort highscores by score property in descending order
+  // displays the scores in descending order, by sorting the array and then listing the array from largest to smallest
   highscores.sort(function(a, b) {
     return b.score - a.score;
   });
 
+  // function to create and append new user scores to the high scores array stored in the local storage array
   highscores.forEach(function(score) {
-    // create li tag for each high score
-    var liTag = document.createElement("li");
-    liTag.textContent = score.initials + " - " + score.score;
+    // creates a list element to append onto the ol tag in the game.html, its position is determined in the previous function
+    var userScores = document.createElement("li");
+    // text content will be the initials value in the score array and then the score value in the score array
+    userScores.textContent = score.initials + " - " + score.score + " 🏅";
 
-    // display on page
-    var olEl = document.getElementById("highscores");
-    olEl.appendChild(liTag);
+    // this displays it on the page by actually appending the new element to the ol tag
+    var highscoresList = document.getElementById("highscores-list");
+    highscoresList.appendChild(userScores);
   });
 }
 
+// this function will clear the highscores information stored in the local storage, emptying the array
 function clearHighscores() {
   window.localStorage.removeItem("highscores");
   window.location.reload();
 }
-
+// when the clear button is clicked, the clearHighScores() function will call and the high scores will be purged from local storage
+// this occasionally flags as null in the console, but the scores are cleared from local storage when it's clicked anyway (double checked)
 document.getElementById("clear").onclick = clearHighscores;
 
-// run function when page loads
+// this displays either the stored array or the empty array, depending on conditions when the page is
 printHighscores();

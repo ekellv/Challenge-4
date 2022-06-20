e6ca770304c0bbcd812607dd784874507a9076b1
Erin V <erinkvoelker@gmail.com>
2022-06-19 21:48:52 -0400

    fixed grammar, spelling and capitalizations, quickly commented explanation of array contained within the file

--- JS/questions.js
+++ JS/questions.js
@@ -1,85 +1,89 @@
+// this array stores all the questions to be called during the quiz
+// each set of questions holds a title with a question, their choices, and an answer which matches one of the choices
+// the quiz.js file can access each of these elements to form the quiz display and ascertain whether or not the user has chosen a correct or incorrect answer
+
 var questions = [
     {
       title: "Which one is a looping structure in JavaScript?",
-      choices: ["All the below", "For", "While", "do-while loops"],
-      answer: "All the below"
+      choices: ["For", "While", "Do-While Loops", "All of the Above"],
+      answer: "All of the Above"
     },
     {
       title: "What are the two basic groups of data types in JavaScript?",
       choices: [
-        "Primitive and attribute",
-        "Primitive and reference types",
-        "Reference types and attribute",
-        "None of the above"
+        "Primitive and Attribute",
+        "Primitive and Reference Types",
+        "Reference Types and Attribute",
+        "None of the Above"
       ],
-      answer: "Primitive and reference types"
+      answer: "Primitive and Reference Types"
     },
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      title: "Commonly used data types DON'T include:",
+      choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+      answer: "Alerts"
     },
     {
-      title: "Boolean operators that can be used in JavaScript include:",
+      title: "Boolean operators used in JavaScript include:",
       choices: [
-        "'And' Operator &&",
-        "'Or' Operator ||",
-        "'Not' Operator !",
-        "All the above"
+        "The 'And' Operator &&",
+        "The 'Or' Operator ||",
+        "The 'Not' Operator !",
+        "All of the Above"
       ],
-      answer: "All the above"
+      answer: "All of the Above"
     },
     {
       title:
-        "Which one of these is not among the three different types of errors in JavaScript?",
+        "Which one of these is not among the three types of errors in JavaScript?",
       choices: [
-        "Animation time errors",
-        "Load time errors",
-        "Run time errors",
+        "Animation Time Errors",
+        "Load Time Errors",
+        "Run Time Errors",
         "Logical Errors"
       ],
-      answer: "Animation time errors"
+      answer: "Animation Time Errors"
     },
     {
-      title: "What is the data type of variables in JavaScript?",
+      title: "In JavaScript, which of these is amongst the data type of variables?",
       choices: [
-        "Object data types",
-        "Function data type",
-        "None of the above",
-        "All of the above"
+        "Object Data Types",
+        "Function Data Types",
+        "Neither of These",
+        "Both of These"
       ],
-      answer: "Object data types"
+      answer: "Object Data Types"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      title: "In an if / else statement the condition must be enclosed within what?",
+      choices: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+      answer: "Parentheses"
     },
     {
-      title: "Arrays in JavaScript can be used to store ____.",
+      title: "What can JavaScript arrays be used to store?",
       choices: [
-        "numbers and strings",
-        "other arrays",
-        "booleans",
-        "all of the above"
+        "Numbers and Strings",
+        "Other Arrays",
+        "Booleans",
+        "All of the Above"
       ],
-      answer: "all of the above"
+      answer: "All of the Above"
     },
     {
       title:
-        "String values must be enclosed within ____ when being assigned to variables.",
-      choices: ["commas", "curly brackets", "quotes", "parentheses"],
-      answer: "quotes"
+        "What must you enclose string values within to assign them to variables?",
+      choices: ["Commas", "Curly brackets", "Quotes", "Parentheses"],
+      answer: "Quotes"
     },
     {
       title:
-        "A very useful tool used during development and debugging for printing content to the debugger is:",
-      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        "What is a useful tool to help you develop and debug code as you write?",
+      choices: ["JavaScript", "Terminal/Git Bash", "For Loops", "console.log"],
       answer: "console.log"
     },
     {
-      title: "What is the type of Pop up boxes available in JavaScript?:",
-      choices: ["Alert", "Confirm", "Prompt", "All the above"],
-      answer: "All the above"
+      title: "What type of pop-ups are available in JavaScript?",
+      choices: ["Alert", "Confirm", "Prompt", "All of the Above"],
+      answer: "All of the Above"
     }
   ];
